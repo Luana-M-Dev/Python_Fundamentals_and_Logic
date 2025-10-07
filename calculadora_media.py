c7e515e812571b6448851fc6a883f7bb0a18504a
Luana Maria da Silva <luanamariasuecia@gmail.com>
2025-10-07 21:16:57 +0200

    feat: Initial commit with Media Calculator project.

---
+++ calculadora_media.py
@@ -0,0 +1,30 @@
+#%%
+
+#Seu Primeiro Projeto: Calculadora de Média 
+#O Desafio
+#Crie um programa que gerencie a entrada de notas de um aluno, calcule a média final das notas inseridas e, por fim, 
+# determine e exiba a situação final do aluno (Aprovado, Reprovado ou Recuperação).
+
+notas = []
+soma_notas = 0
+
+while True:
+    nota = input("Digite a nota do aluno:")
+    if nota == "":
+        break
+    notas.append(float(nota))
+    soma_notas = soma_notas + float(nota)
+
+if len(notas) == 0:
+    print("Nenhuma nota inserida. Encerrando.")
+    exit()
+
+media = soma_notas / len(notas)
+
+
+if media >= 7:
+    print(f"O aluno está APROVADO! A média dele foi {media}")
+elif media >=5:
+    print(f"O aluno está de RECUPERAÇÃO! A média dele foi {media}")
+else:
+    print(f"O aluno está REPROVADO! A média dele foi {media}")
